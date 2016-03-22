ea16df6d6df795c1403243d6e85b0d90f80d1151
qrempla <q.rempla.phone@gmail.com>
2016-03-22 14:06:23 +0100

    Tests Unitaires ScoreComponent

--- project/src/main/java/com/esiea/tetris/communication/MessageBus.java
+++ project/src/main/java/com/esiea/tetris/communication/MessageBus.java
@@ -2,13 +2,10 @@ package com.esiea.tetris.communication;
 
 import net.engio.mbassy.bus.MBassador;
 
-public class MessageBus {
-    private static MBassador bus;
+public final class MessageBus {
+    private final static MBassador bus = new MBassador();
     
     public static MBassador getInstance(){
-        if(bus == null){
-            bus = new MBassador();
-        }
         return bus;
     }
 }
