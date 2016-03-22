1a3c3bb438e4014cbcb34e2c7db4717dddc4c123
qrempla <q.rempla.phone@gmail.com>
2016-03-22 16:01:50 +0100

    Création classe TetriminoMover -> gestion des changements d'état du Tetrimino en fonction des actions
    Modifictaion classe Tetrimino
    -> gestion de l'orientation du Tetrimino et des différentes
    configurations visuelles associées
    
    Modification classe vec2
    -> ajout de méthodes permettant l'addition d'un vecteur

--- project/src/main/java/com/esiea/tetris/utils/vec2.java
+++ project/src/main/java/com/esiea/tetris/utils/vec2.java
@@ -1,11 +1,23 @@
 package com.esiea.tetris.utils;
 
-public class vec2<T>{
-    public T x;
-    public T y;
+public class vec2{
+    public double  x;
+    public double  y;
     
-    public vec2(T _x, T _y){
+    public vec2(double _x, double _y){
         this.x = _x;
         this.y = _y;
+    }
+    
+    public void add(vec2 v)
+    {
+    	this.x+=v.x;
+    	this.y+=v.y;
+    }
+    
+    public void add(double x, double y)
+    {
+    	this.x+=x;
+    	this.y+=y;
     }
 }
--- project/src/test/com/esiea/tetris/model/concrete/TestScoreComponent.java
+++ project/src/test/com/esiea/tetris/model/concrete/TestScoreComponent.java
@@ -21,25 +21,36 @@ public class TestScoreComponent {
 	@Test
 	public void itShouldIncrementScoreForOneLineForPlayerOne()
 	{
+		int initialScore=sc.getScore();
+		
+		// Envoi d'une notification informant que le joueur 1 a complété 1 ligne
 		LineNotification msg= new LineNotification(1,1);
 		MessageBus.getInstance().post(msg);
 		
-		assertThat(sc.getScore()==100);
+		// ... le message est traité automatiquement par le ScoreComponent depuis sa méthode Handle 
+		
+		// On vérifie que la valeur du score a été modifiée correctement
+		assertThat(sc.getScore()==initialScore+100);
 	}
 	
 	@Test
 	public void itShouldIncrementScoreForTwoLinesForPlayerOne()
 	{
+		int initialScore=sc.getScore();
+		
+		// Envoi d'une notification informant que le joueur 1 a complété 2 lignes
 		LineNotification msg= new LineNotification(1,2);
 		MessageBus.getInstance().post(msg);
 		
-		assertThat(sc.getScore()==400);
+		assertThat(sc.getScore()==initialScore+400);
 	}
 	
 	@Test
 	public void itShouldNotIncrementScoreForWrongPlayer()
 	{
 		int initialScore=sc.getScore();
+		
+		// Envoi d'une notificaiton qui s'adresse à un autre joueur (ici 4), le joueur testé (ici 1) ne devra donc pas incrémenter son score
 		LineNotification msg= new LineNotification(4,2);
 		MessageBus.getInstance().post(msg);
 		
