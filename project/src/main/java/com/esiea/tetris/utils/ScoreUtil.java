bd95708f1443d643a8a629ad81fbe2b189125888
qrempla <q.rempla.phone@gmail.com>
2016-04-13 21:58:37 +0200

    Modification fonction de ScoreUtil
    Finalisation des tests sur ScoreUtil

--- project/src/main/java/com/esiea/tetris/utils/ScoreUtil.java
+++ project/src/main/java/com/esiea/tetris/utils/ScoreUtil.java
@@ -11,7 +11,17 @@ import java.util.logging.Logger;
 
 public class ScoreUtil {
 	
-    private static final String filename = "scores.txt";
+    private static String filename = "scores.txt";
+    
+    public static void changeFilename(String name)
+    {
+    	filename=name;
+    }
+    
+    public static String getFilename()
+    {
+    	return filename;
+    }
 	
     public static int[] getHighScores(){
         int[] scores = new int[]{0,0,0,0,0};
@@ -38,51 +48,51 @@ public class ScoreUtil {
     	
     	int[] scores = getHighScores();
     	
-    	String content="";
+    	boolean decalerScore=true;
     	
-    	int indiceToChange = 0;
-    	boolean decalerScore=false;
-    	
-    	for(int i=0; i<5;i++) // on parcourt le tableau dans l'ordre des scores décroissants
+    	if(score>scores[4]) // on vérifie rapidement si le score peut entrer dans le classement
     	{
-            if(decalerScore) // si le score du joueur doit s'insérer dans le classement, il faut décaler les autres
-            {
-                scores[i]=scores[i-1];
-            }
-            else if(score>scores[i]) // si le score testé est moins bon que celui du joueur
-            {
-                indiceToChange=i; // on sauvegarde l'emplacement du score du joueur
-                decalerScore=true;
-            }
-    	}
-    	
-    	
-    	if(decalerScore){
-            scores[indiceToChange]=score;
-
-            for(int i=0; i<5;i++){
-                System.out.println(scores[i]);
-            }
-
-            // Réécriture du contenu du fichier avec le nouveau score
-
-            BufferedWriter writer;
-            FileWriter fileWriter;
-            try {
-                fileWriter = new FileWriter(filename);
-                writer = new BufferedWriter(fileWriter);
-                String line;
-
-                for(int i=0; i<5;i++){
-                    writer.write(String.valueOf(scores[i]));
-                    writer.newLine();
-                }
-                
-                fileWriter.close();
-                writer.close();
-            } catch (Exception ex){
-                Logger.getLogger(ScoreUtil.class.getName()).log(Level.SEVERE, null, ex);
-            }
+    		
+	    	for(int i=4; i>=0;i--) // on parcourt le tableau dans l'ordre des scores croissants
+	    	{
+	    		if(decalerScore)
+	    		{
+		    		if(i>0)
+		    		{
+		    			if(scores[i-1]<score) // Si le score au-dessus est moins bon que celui du joueur, on le décale en bas
+		    			{
+		    				scores[i]=scores[i-1];
+		    			}
+		    			else // si au-dessus est meilleur que celui du joueur, on insère le score du joueur dans le tableau, et on arrête la procédure de décalage
+		    			{
+		    				scores[i]=score;
+		    				decalerScore=false;
+		    			}
+		    		}
+		    		else scores[0]=score;
+	    		}
+	    		
+	    	}
+	
+	
+	            // Réécriture du contenu du fichier avec le nouveau score
+	
+	            BufferedWriter writer;
+	            FileWriter fileWriter;
+	            try {
+	                fileWriter = new FileWriter(filename);
+	                writer = new BufferedWriter(fileWriter);
+	
+	                for(int i=0; i<5;i++){
+	                    writer.write(String.valueOf(scores[i]));
+	                    writer.newLine();
+	                }
+	
+	                writer.close();
+	                fileWriter.close();
+	            } catch (Exception ex){
+	                Logger.getLogger(ScoreUtil.class.getName()).log(Level.SEVERE, null, ex);
+	            }
 
         }
 
--- project/src/test/java/com/esiea/tetris/utils/TestScoreUtil.java
+++ project/src/test/java/com/esiea/tetris/utils/TestScoreUtil.java
@@ -2,19 +2,197 @@ package com.esiea.tetris.utils;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestScoreUtil {
+	
+	String savedFilename;
+	String filename="test.txt";
 
 	@Before
 	public void init()
 	{
+		// on récupère le nom du fichier de score d'origine
+		savedFilename=ScoreUtil.getFilename();
+		// on utilise un autre fichier de score factice pour les tests, afin de ne pas altérer l'original
+		ScoreUtil.changeFilename(filename);
+		
+	}
+	
+	public void initFile()
+	{
+		BufferedWriter writer;
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter(filename);
+            writer = new BufferedWriter(fileWriter);
+
+            writer.write("1000");
+            writer.newLine();
+            writer.write("700");
+            writer.newLine();
+            writer.write("500");
+            writer.newLine();
+            writer.write("300");
+            writer.newLine();
+            writer.write("100");
+            
+
+            writer.close();
+            fileWriter.close();
+        } catch (Exception ex){
+            Logger.getLogger(TestScoreUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	}
+	
+	public void deleteFile()
+	{
+		// on supprime le fichier de score créé pour les tests
+		(new File(filename)).delete();
+	}
+	
+	
+	@Test
+	public void itShouldWriteTheContentOfTheFileInAnArray() {
+		
+		initFile(); // on réinitialise le fichier car on ne peut pas gèrer l'ordre d'exécution des tests
+		
+		int [] scoresToCheck=ScoreUtil.getHighScores();
+		int[] scores={1000,700,500,300,100};
+		
+		for(int i=0;i<5;i++)
+		{
+			assertEquals(scores[i],scoresToCheck[i]);
+		}
+		
+		deleteFile();
+	}
+	
+	
+	@Test
+	public void itShouldNotInsertTheScoreOfThePlayerInTheFileWhenTheScoreIsNotGoodEnough() {
+		
+		initFile(); // on réinitialise le fichier car on ne peut pas gèrer l'ordre d'exécution des tests
+		
+		// On essaie d'insérer le score 50, mais celui-ci n'est pas assez bon pour s'insérer dans le classement
+		ScoreUtil.writeHighScore(50);
+		
+		int [] scoresToCheck=ScoreUtil.getHighScores();
+		int[] scores={1000,700,500,300,100};
+		
+		for(int i=0;i<5;i++)
+		{
+			assertEquals(scores[i],scoresToCheck[i]);
+		}
+		
+		deleteFile();
+		
+	}
+	
+	
+	@Test
+	public void itShouldInsertTheScoreOfThePlayerInTheFileWhenTheScoreIsGoodEnoughToBeAtFirstPlace() {
+		
+		initFile(); // on réinitialise le fichier car on ne peut pas gèrer l'ordre d'exécution des tests
+		
+		
+		// On essait d'insérer le score 800, qui est assez bon pour entrer dans le classement
+		ScoreUtil.writeHighScore(1500);
+		
+		int [] scoresToCheck=ScoreUtil.getHighScores();
+		//int[] scores={1000,700,500,300,100};
+		int[] scores={1500,1000,700,500,300};
+		
+		for(int i=0;i<5;i++)
+		{
+			assertEquals(scores[i],scoresToCheck[i]);
+		}
+		
+		deleteFile();
+		
 	}
 	
 	@Test
-	public void test() {
-		ScoreUtil.writeHighScore(2500);
+	public void itShouldInsertTheScoreOfThePlayerInTheFileWhenTheScoreIsGoodEnoughToBeAtSecondPlace() {
+		
+		initFile(); // on réinitialise le fichier car on ne peut pas gèrer l'ordre d'exécution des tests
+		
+		
+		// On essait d'insérer le score 800, qui est assez bon pour entrer dans le classement
+		ScoreUtil.writeHighScore(800);
+		
+		int [] scoresToCheck=ScoreUtil.getHighScores();
+		//int[] scores={1000,700,500,300,100};
+		int[] scores={1000,800,700,500,300};
+		
+		for(int i=0;i<5;i++)
+		{
+			assertEquals(scores[i],scoresToCheck[i]);
+		}
+		
+		deleteFile();
+		
+	}
+	
+	@Test
+	public void itShouldInsertTheScoreOfThePlayerInTheFileEvenWhenTheScoreIsTheSameThanAnother() {
+		
+		initFile(); // on réinitialise le fichier car on ne peut pas gèrer l'ordre d'exécution des tests
+		
+		
+		// On essait d'insérer le score 800, qui est assez bon pour entrer dans le classement
+		ScoreUtil.writeHighScore(500);
+		
+		int [] scoresToCheck=ScoreUtil.getHighScores();
+		//int[] scores={1000,700,500,300,100};
+		int[] scores={1000,700,500,500,300};
+		
+		for(int i=0;i<5;i++)
+		{
+			assertEquals(scores[i],scoresToCheck[i]);
+		}
+		
+		deleteFile();
+		
+	}
+	
+	@Test
+	public void itShouldInsertTheScoreOfThePlayerInTheFileWhenTheScoreIsGoodEnoughToBeAtLastPlace() {
+		
+		initFile(); // on réinitialise le fichier car on ne peut pas gèrer l'ordre d'exécution des tests
+		
+		
+		// On essait d'insérer le score 800, qui est assez bon pour entrer dans le classement
+		ScoreUtil.writeHighScore(150);
+		
+		int [] scoresToCheck=ScoreUtil.getHighScores();
+		//int[] scores={1000,700,500,300,100};
+		int[] scores={1000,700,500,300,150};
+		
+		for(int i=0;i<5;i++)
+		{
+			assertEquals(scores[i],scoresToCheck[i]);
+		}
+		
+		deleteFile();
+		
+	}
+	
+	
+	@After
+	public void end()
+	{
+		// on réinitialise le nom du fichier de score
+		ScoreUtil.changeFilename(savedFilename);
+		
 	}
 
 }
